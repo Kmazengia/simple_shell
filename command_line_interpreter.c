6aecea0df25a515401883809005f5c2a1234580a
AHelen19 <ayelehelena16@gmail.com>
2022-11-14 08:29:20 -0800

    adding pheudocode

---
+++ command_line_interpreter.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * main - fork & wait example
+ *
+ *  Return: Always 0.
+ **/
+void main(void){
+	char *buffer = mallock(1124)
+	
+	printf("Enter a number");
+	wait()
+	pid_t chikd_pid = fork();
+	if (child_pid == 0){
+	 getline(buffer, len, stdin);
+	 
+	//prompt implementation
+	//readline
+	// getline(buffer, len ,stdin);
+	// based on the written command
+	// run execve givenCommand
+	//wait(till user type a command);
+	//execute given command
+	//repeat the above
+

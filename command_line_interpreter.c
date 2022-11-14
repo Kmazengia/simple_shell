64df4099395d474b05679e57c55f7419623bff19
AHelen19 <ayelehelena16@gmail.com>
2022-11-14 10:55:41 -0800

    reading line of code

--- command_line_interpreter.c
+++ command_line_interpreter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 /**
@@ -7,21 +8,29 @@
  *
  *  Return: Always 0.
  **/
-void main(void){
-	char *buffer = mallock(1124)
-	
-	printf("Enter a number");
-	wait()
-	pid_t chikd_pid = fork();
-	if (child_pid == 0){
-	 getline(buffer, len, stdin);
-	 
-	//prompt implementation
-	//readline
-	// getline(buffer, len ,stdin);
+int main(void)
+
+{
+
+  printf("Please enter a line:\n");
+
+  char *line;
+  line = (char *)malloc(1020 * sizeof(char));
+
+  size_t len = 0;
+
+  ssize_t lineSize = 0;
+
+  lineSize = getline(&line, &len, stdin);
+
+  printf("You entered %s, which has %zu chars.\n", line, lineSize -1);
+
+  free(line);
+
+  return 0;
 	// based on the written command
 	// run execve givenCommand
 	//wait(till user type a command);
 	//execute given command
 	//repeat the above
-
+}
